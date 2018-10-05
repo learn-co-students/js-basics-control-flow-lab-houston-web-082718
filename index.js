192773b82e43b56827dc840a70da8e7cde706985
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-05 17:33:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+let params
+
+function scuberGreetingForFeet(params) {
+  if (params <= 400) {
+    return 'This one is on me!';
+  } else if (params > 2000 && params <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (params > 2500) {
+    return 'No can do.';
+  }
+};
+
+function ternaryCheckCity(params) {
+  return params === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+};
+
+function switchOnCharmFromTip(params) {
+  switch (params) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+
+    default: return 'Bye.'
+      break;
+  }
+};
